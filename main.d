7590b3fc1f0a7d31529cb5355b7a61dbd2e85984
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-10-02 15:51:59 -0700

    Trying from scratch C version

--- main.d
+++ main.d
@@ -112,139 +112,6 @@ void InitDerelict() {
 	}
 }
 
-/*
-float width = 200, height = 200;
-float bpp = 0;
-float near = 10.0, far = 100000.0, fovy = 45.0;
-float[3] position = [0,0,-40];
-const float[9] triangle = [
-	  0,  10, 0,  // top point
-	-10, -10, 0,  // bottom left
-	 10, -10, 0   // bottom right
-];
-float rotate_degrees  = 90;
-float[3] rotate_axis = [0,1,0];
-
-SDL_Surface* screen = null;
-*/
-public char* toSZ(S)(S value)
-if(isSomeString!S) {
-	import std.string : toStringz;
-	return cast(char*)toStringz(value);
-}
-
-string GetSDLError() {
-	import std.string : fromStringz;
-	return cast(string) fromStringz(SDL_GetError());
-}
-
-bool IsSurfaceRGBA8888(const SDL_Surface* surface) {
-	return (surface.format.Rmask == 0xFF000000 &&
-			surface.format.Gmask == 0x00FF0000 &&
-			surface.format.Bmask == 0x0000FF00 &&
-			surface.format.Amask == 0x000000FF);
-}
-
-SDL_Surface* EnsureSurfaceRGBA8888(SDL_Surface* surface) {
-	import std.string : format;
-
-	// Just return if it is already RGBA8888
-	if (IsSurfaceRGBA8888(surface)) {
-		return surface;
-	}
-
-	// Convert the surface into a new one that is RGBA8888
-	SDL_Surface* new_surface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA8888, 0);
-	if (new_surface == null) {
-		throw new Exception("Failed to convert surface to RGBA8888 format: %s".format(GetSDLError()));
-	}
-	SDL_FreeSurface(surface);
-
-	// Make sure the new surface is RGBA8888
-	if (! IsSurfaceRGBA8888(new_surface)) {
-		throw new Exception("Failed to convert surface to RGBA8888 format: %s".format(GetSDLError()));
-	}
-	return new_surface;
-}
-
-SDL_Surface* LoadSurface(const string file_name) {
-	import std.file : exists;
-	import std.string : format;
-
-	string complete_name = file_name;
-	if (! exists(complete_name)) {
-		throw new Exception("File does not exist: %s".format(complete_name));
-	}
-
-	SDL_Surface* surface = IMG_Load(complete_name.toSZ);
-	if (surface == null) {
-		throw new Exception("Failed to load surface \"%s\": %s".format(file_name, GetSDLError()));
-	}
-
-	if (surface.format.BitsPerPixel < 32) {
-		throw new Exception("Image has no alpha channel \"%s\"".format(file_name));
-	}
-
-	surface = EnsureSurfaceRGBA8888(surface);
-
-	return surface;
-}
-/*
-void render() {
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glLoadIdentity();
-
-	glTranslatef(position[0], position[1], position[2]);
-
-	{
-		static Uint32 last = 0;
-		static float angle = 0;
-
-		Uint32 now = SDL_GetTicks();
-		float delta = (now - last) / 1000.0f; // in seconds
-		last = now;
-
-		angle += rotate_degrees * delta;
-
-		// c modulo operator only supports ints as arguments
-		auto MOD(T)(T n, T d) { return (n - (d * cast(int) ( n / d ))); }
-		angle = MOD( angle, 360 );
-
-		glRotatef( angle, rotate_axis[0], rotate_axis[1], rotate_axis[2] );
-	}
-
-	glBegin(GL_TRIANGLES);
-	for (int i=0; i<=6; i+=3) {
-		glColor3f(i==0, i==3, i==6); // adding some color
-		glVertex3fv(&triangle[i]);
-	}
-	glEnd();
-
-	SDL_GL_SwapBuffers();
-}
-*/
-
-
-
-extern (C) void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) nothrow {
-	try {
-		stderr.writefln("key:%s, scancode:%s, action:%s, mods:%s", key, scancode, action, mods); stderr.flush();
-	} catch (Throwable) {
-	}
-
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-}
-
-extern (C) void error_callback(int error, const(char)* description) nothrow {
-	try {
-		stderr.writefln("error: %s", description); stderr.flush();
-	} catch (Throwable) {
-	}
-}
-
 // helper to check and display for shader compiler errors
 bool check_shader_compile_status(GLuint obj) {
     GLint status;
@@ -252,7 +119,7 @@ bool check_shader_compile_status(GLuint obj) {
     if(status == GL_FALSE) {
         GLint length;
         glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &length);
-//        vector<char> log(length);
+//        std::vector<char> log(length);
 //        glGetShaderInfoLog(obj, length, &length, &log[0]);
 //        std::cerr << &log[0];
         return false;
@@ -267,7 +134,7 @@ bool check_program_link_status(GLuint obj) {
     if(status == GL_FALSE) {
         GLint length;
         glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &length);
-//        vector<char> log(length);
+//        std::vector<char> log(length);
 //        glGetProgramInfoLog(obj, length, &length, &log[0]);
 //        std::cerr << &log[0];
         return false;
@@ -276,51 +143,30 @@ bool check_program_link_status(GLuint obj) {
 }
 
 int main() {
+	InitDerelict();
+
 	int width = 640;
     int height = 480;
 
-	stdout.writefln("InitDerelict ..."); stdout.flush();
-	InitDerelict();
-/*
-	// Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		stderr.writefln("Could not initialize SDL: %s", SDL_GetError());
-		return 1;
-	}
-*/
-	stdout.writefln("glfwInit ..."); stdout.flush();
-	if (! glfwInit()) {
-		return 1;
-	}
+    if(glfwInit() == GL_FALSE) {
+        stderr.writefln("failed to init GLFW"); stderr.flush();
+        return 1;
+    }
 
-	// select opengl version
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // select opengl version
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-	stdout.writefln("glfwSetErrorCallback ..."); stdout.flush();
-	glfwSetErrorCallback(&error_callback);
+    // create a window
+    GLFWwindow *window;
+    if((window = glfwCreateWindow(width, height, "03texture", null, null)) == null) {
+        stderr.writefln("failed to open window"); stderr.flush();
+        glfwTerminate();
+        return 1;
+    }
 
-	stdout.writefln("window ..."); stdout.flush();
-	/* Create a windowed mode window and its OpenGL context */
-	GLFWwindow* window = glfwCreateWindow(width, height, "03texture", null, null);
-	if (!window) {
-		glfwTerminate();
-		return 1;
-	}
-
-	glfwSetKeyCallback(window, &key_callback);
-
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
-/*
-	if(glxwInit()) {
-		stderr.writefln("Failed to init GL3W"); stderr.flush();
-		glfwDestroyWindow(window);
-		glfwTerminate();
-		return 1;
-	}
-*/
 
 	// shader source code
 	string vertex_source =
@@ -349,19 +195,17 @@ int main() {
 	// program and shader handles
     GLuint shader_program, vertex_shader, fragment_shader;
 
-	// we need these to properly pass the strings
+    // we need these to properly pass the strings
     char* source;
     int length;
 
-	stdout.writefln("FIXME: Fails on glCreateShader ..."); stdout.flush();
+    // create and compiler vertex shader
 	stdout.writefln("glCreateShader ..."); stdout.flush();
-	// create and compiler vertex shader
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    source = cast(char*)vertex_source;
+	stdout.writefln("Main loop ..."); stdout.flush();
+    source = cast(char*) vertex_source;
     length = cast(int) vertex_source.length;
-	stdout.writefln("glShaderSource ..."); stdout.flush();
     glShaderSource(vertex_shader, 1, &source, &length);
-	stdout.writefln("glCompileShader ..."); stdout.flush();
     glCompileShader(vertex_shader);
     if(! check_shader_compile_status(vertex_shader)) {
         glfwDestroyWindow(window);
@@ -369,131 +213,7 @@ int main() {
         return 1;
     }
 
-	/* Loop until the user closes the window */
-	while (! glfwWindowShouldClose(window)) {
-			stdout.writefln("loop ..."); stdout.flush();
-			/* Render here */
-/*
-			glClearColor( 1.0f, 0.0f, 0.0f, 0.0f );
-			glClear(GL_COLOR_BUFFER_BIT);
+	stdout.writefln("Main loop ..."); stdout.flush();
 
-			glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
-	   glLoadIdentity(); //Reset the drawing perspective
-	     glTranslatef(0.0f,0.0f,-35.0f); //Translate whole scene to -ve z-axis by -35 unit
-
-	     GLuint text2D;
-	     text2D = LoadTexture("cicb.tga"); //loading image for texture
-
-	     glEnable(GL_TEXTURE_2D); //Enable texture
-	     glBindTexture(GL_TEXTURE_2D,text2D);//Binding texture
-	     glPushMatrix();
-	     glBegin(GL_POLYGON); //Begin quadrilateral coordinates
-	     glNormal3f(0.0f, 0.0f, 1.0f);//normal vector
-	   glTexCoord2f(0.0f, 0.0f); //Texture co-ordinate origin or  lower left corner
-	   glVertex3f(-10.0f,-11.0f,5.0f);
-	   glTexCoord2f(1.0f, 0.0f); //Texture co-ordinate lower right corner
-	   glVertex3f(10.0f,-11.0f,5.0f);
-	   glTexCoord2f(1.0f, 1.0f);//Texture co-ordinate top right corner
-	   glVertex3f(10.0f,-1.0f,-15.0f);
-	   glTexCoord2f(0.0f, 1.0f);//Texture co-ordinate top left corner
-	   glVertex3f(-10.0f,-1.0f,-15.0f);
-	   glEnd(); //End quadrilateral coordinates
-
-	   glPopMatrix();
-
-	   glDisable(GL_TEXTURE_2D);
-
-
-
-	     glEnable(GL_TEXTURE_2D);
-	     glBindTexture(GL_TEXTURE_2D,text2D);
-	     glPushMatrix();
-	     glBegin(GL_POLYGON);
-	     glNormal3f(0.0f, 0.0f, 1.0f);
-	   	glTexCoord2f(0.0f, 0.0f);//Texture co-ordinate origin or lower left corner
-	     glVertex3f(-10.0f,-1.0f,-15.0f);
-	     glTexCoord2f(10.0f, 0.0f); //Texture co-ordinate for repeating image ten times form
-	     //origin to lower right corner
-	     glVertex3f(10.0f,-1.0f,-15.0f);
-	     glTexCoord2f(10.0f, 10.0f);//repeat texture ten times form lower to top right corner.
-	     glVertex3f(10.0f,15.0f,-15.0f);
-	     glTexCoord2f(0.0f, 10.0f);//repeat texture ten time form top right to top left corner.
-	     glVertex3f(-10.0f,15.0f,-15.0f);
-	     glEnd();
-	     glPopMatrix();
-	     glDisable(GL_TEXTURE_2D); //Disable the texture
-*/
-			/* Swap front and back buffers */
-			glfwSwapBuffers(window);
-
-			/* Poll for and process events */
-			glfwPollEvents();
-	}
-
-	glfwTerminate();
-
-/*
-	// Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
-		return 1;
-	}
-
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1 );
-
-	screen = SDL_SetVideoMode(
-		width, height, bpp,
-		SDL_ANYFORMAT | SDL_OPENGL );
-
-	glViewport(0, 0, width, height);
-
-	glPolygonMode( GL_FRONT, GL_FILL );
-	glPolygonMode( GL_BACK,  GL_LINE );
-
-	glMatrixMode(GL_PROJECTION);
-	gluPerspective(fovy,width/height,near,far);
-
-	glMatrixMode(GL_MODELVIEW);
-
-	// ===================
-	// Texture 2
-	// ===================
-	GLuint texture2;
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	// Set our texture parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// Set texture filtering
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// Load, create texture and generate mipmaps
-	SDL_Surface* surface = null;
-	try {
-		//LoadSurface("awesomeface.png");
-	} catch (Throwable err) {
-		//std::exception_ptr err = std::current_exception();
-		stderr.writefln("!!! %s", err);
-		return 1;
-	}
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface.w, surface.h, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, surface.pixels);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	SDL_FreeSurface(surface);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-
-	while (true) {
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-				case SDL_QUIT:
-					SDL_Quit();
-					break;
-			}
-		}
-
-		render();
-	}
-*/
 	return 0;
 }
